185702b6dd01c9bba45e372c330c60bc7314df61
Sammidysam <sammidysam@gmail.com>
2015-07-21 22:10:32 -0400

    Add example

---
+++ examples/combo.rb
@@ -0,0 +1,24 @@
+class Combo
+	def initialize(tune, composer = nil, arranger = nil)
+		@tune = tune
+		@variable_prefix = tune.downcase.split(" ").map { |w| w.capitalize }.join
+		@variable_prefix[0] = @variable_prefix[0].downcase
+		@filename = tune.downcase.split(" ").join("-")
+
+		@composer = composer
+		@arranger = arranger
+
+		@version = lilypond_version
+	end
+
+	def lilypond_version
+		# Get the lilypond version text.
+		text = `lilypond --version`
+
+		# The version number is on the first line, after "GNU LilyPond".
+		version_line = text.lines.first
+
+		# Conveniently, it is also the last word.
+		version_line.split(" ").last
+	end
+end
